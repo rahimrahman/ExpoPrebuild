350b25c924109fc896911aebea577ce3dcb9ac2f
Rahim Rahman <rahim.rahman@mattermost.com>
2024-08-21 05:58:16 -0600

    after prebuild command

--- android/app/proguard-rules.pro
+++ android/app/proguard-rules.pro
@@ -1,4 +1,5 @@
-# Add project specific ProGuard rules here.
+
+      # Add project specific ProGuard rules here.
 # By default, the flags in this file are appended to flags specified
 # in /usr/local/Cellar/android-sdk/24.3.3/tools/proguard/proguard-android.txt
 # You can edit the include path and order by changing the proguardFiles
@@ -12,3 +13,6 @@
 -keep class com.facebook.react.turbomodule.** { *; }
 
 # Add any project specific keep options here:
+
+      -keep class com.nozbe.watermelondb.** { *; }
+      
--- android/app/src/main/java/com/anonymous/ExpoPrebuild/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/ExpoPrebuild/MainApplication.kt
@@ -1,6 +1,10 @@
 package com.anonymous.ExpoPrebuild
 
+
 import android.app.Application
+import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage;
+import com.facebook.react.bridge.JSIModulePackage;        
+
 import android.content.res.Configuration
 
 import com.facebook.react.PackageList
@@ -31,7 +35,11 @@ class MainApplication : Application(), ReactApplication {
           override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
           override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+          
+        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        override fun getJSIModulePackage(): JSIModulePackage {
+        return WatermelonDBJSIPackage()
+        }
       }
   )
 
